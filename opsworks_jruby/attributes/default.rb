962b154d348f6de8bd5a08bd6a6e6646fb4ec6c0
xueshibing <xueshibing@gmail.com>
2015-01-14 14:09:44 +0800

    fix jruby_path

--- opsworks_jruby/attributes/default.rb
+++ opsworks_jruby/attributes/default.rb
@@ -1,3 +1,4 @@
 default['opsworks_jruby']['version'] = "1.7.10"
 default['opsworks_jruby']['home_base_dir'] = "/usr/local"
 default['opsworks_jruby']['JRUBY_OPTS'] = "--2.0"
+default['opsworks_jruby']['jruby_path'] = "#{node['opsworks_jruby']['home_base_dir']}/jruby-#{node['opsworks_jruby']['version']}/bin"
