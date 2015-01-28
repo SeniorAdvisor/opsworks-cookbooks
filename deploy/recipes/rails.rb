e54cf1d2dcc2187a265f100508b626b687036237
xueshibing <xueshibing@gmail.com>
2015-01-28 11:03:41 +0800

    export environment variables

--- deploy/recipes/rails.rb
+++ deploy/recipes/rails.rb
@@ -13,6 +13,12 @@ node[:deploy].each do |application, deploy|
     path deploy[:deploy_to]
   end
 
+  file "/etc/profile.d/#{application}.sh" do
+    content deploy[:environment_variables].collect{|k,v|"export #{k}=#{v}"}.join("\n")
+    mode 0755
+    only_if { deploy[:export_environment_variables] && deploy[:environment_variables].is_a?(::Hash)}
+  end
+
   opsworks_rails do
     deploy_data deploy
     app application
